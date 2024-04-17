1dcf45f2fcc7de1c2242038b9b90d2a473901347
SmirnowK <konstantin.smirnoww@gmail.com>
2024-04-17 21:28:12 +0200

    Hwork04

---
+++ src/Hwork04/Hwork04.java
@@ -0,0 +1,18 @@
+package Hwork04;
+
+public class Hwork04 {
+    public static void main(String[] args) {
+
+        int m = 8;
+        int d = 8;
+        int mi = 7;
+        int di = 9;
+        int f = 8;
+        int s = 9;
+        int so = 8;
+
+        int sum = (m+d+mi+di+f+s+so)/7;
+
+        System.out.printf("Srednjaja Temperatura za nedelju: "+sum);
+    }
+}
