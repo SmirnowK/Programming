d67b801b277454f06b4cac355b6fa4785f47f349
SmirnowK <konstantin.smirnoww@gmail.com>
2024-04-17 21:03:13 +0200

    H03Dz3

---
+++ src/dz3/A.java
@@ -0,0 +1,18 @@
+package dz3;
+
+public class A {
+    public static void main(String[] args) {
+
+        int A = 1000;
+
+        int B = 500;
+
+        int skidka = 100;
+
+        int result = (A+B-skidka);
+
+        System.out.println("Summa za oba tovara so skidkoi: "+result);
+
+        System.out.println("Skidka: "+skidka);
+    }
+}
