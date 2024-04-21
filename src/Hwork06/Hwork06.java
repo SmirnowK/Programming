58e30a79b79509ec70229547edadcb2b43ac3c5a
SmirnowK <konstantin.smirnoww@gmail.com>
2024-04-21 19:42:43 +0200

    Hwork06

---
+++ src/Hwork06/Hwork06.java
@@ -0,0 +1,12 @@
+package Hwork06;
+
+public class Hwork06 {
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        int result = (x += x++);;
+
+        System.out.println("The value of x after the operation is: "+result);
+    }
+}
