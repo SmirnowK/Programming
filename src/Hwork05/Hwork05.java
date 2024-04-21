fe05e73b90255ea28a9dc38ba0e846cad8c071cf
SmirnowK <konstantin.smirnoww@gmail.com>
2024-04-21 19:32:54 +0200

    Hwork05

---
+++ src/Hwork05/Hwork05.java
@@ -0,0 +1,13 @@
+package Hwork05;
+
+public class Hwork05 {
+    public static void main(String[] args) {
+
+        final int a = 2;
+        final int b = 3;
+
+        final int result = a % b;
+
+        System.out.println(result);
+    }
+}
